3b5ec14a529fd241f2db419e684f6cac756e5fe8
takafumiTech <t.k.basket.f@gmail.com>
2023-08-29 10:15:11 +0900

    finish

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,10 @@
 class PrototypesController < ApplicationController
+  before_action :same_sentence, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def index
-    @prototype = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -17,17 +21,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user) 
+    @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype.id)
     else
@@ -36,14 +37,23 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
+    @prototype.destroy
     redirect_to root_path
   end
 
   private
 
+  def same_sentence
+    @prototype = Prototype.find(params[:id])
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user.id == @prototype.user.id
+      redirect_to root_path
+    end
+  end 
 end
